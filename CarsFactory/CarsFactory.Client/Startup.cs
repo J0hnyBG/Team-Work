0bb22a715bffca23662d2ec54f354a65a7201f94
KaloyanchoSt <KaloyanchoSt@gmail.com>
2016-10-25 20:20:26 +0300

    Some refac, added Startup Client, added connectionString

---
+++ CarsFactory/CarsFactory.Client/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Cars.Models;
+using Cars.Models.Contracts;
+using Dealership.Data;
+
+namespace Cars.Client
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            var dbContext = new CarsFactoryDbContext();
+            var newCar = new Car
+            {
+                Model = "I"
+            };
+
+            dbContext.Cars.Add(newCar);
+        }
+    }
+}
---
+++ CarsFactory/CarsFactory.Data/DealershipDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Cars.Models;
+using Cars.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dealership.Data
+{
+    public class CarsFactoryDbContext : DbContext
+    {
+        public CarsFactoryDbContext()
+            : base("CarsFactory")
+        {
+
+        }
+
+        public virtual IDbSet<Car> Cars { get; set; }
+
+        public virtual IDbSet<Dealer> Dealers { get; set; }
+
+        public virtual IDbSet<Manufacturer> Manufacturers { get; set; }
+    }
+}
