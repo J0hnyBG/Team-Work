aeeac80aaeb2933a489bba0c154dd4daf084e8d9
Alex Nestorov <nestorov.alexander@gmail.com>
2016-10-28 00:14:37 +0300

    add save data in sql database from mongodb database

--- CarsFactory/CarsFactory.Client/Startup.cs
+++ CarsFactory/CarsFactory.Client/Startup.cs
@@ -44,17 +44,92 @@
             }).Wait();
         }
 
+        // TODO: Refactor method
         private static async Task GetMongoData()
         {
             var repo = new MongoDbRepository();
 
+
+            var towns = (await repo.GetTownsData()).ToList();
+            var platforms = (await repo.GetPlatformsData()).ToList();
+            var orders = (await repo.GetOrdersData()).ToList();
+            var models = (await repo.GetModelsData()).ToList();
+            var manufacturers = (await repo.GetManufacturersData()).ToList();
+            var engines = (await repo.GetEnginesData()).ToList();
+            var dealers = (await repo.GetDealersData()).ToList();
             var cars = (await repo.GetCarsData()).ToList();
 
             var ctx = new CarsFactoryDbContext();
+
+
             Console.WriteLine(cars.Count);
+            Console.WriteLine(models.Count);
+            Console.WriteLine(manufacturers.Count);
+            Console.WriteLine(orders.Count);
+            Console.WriteLine(platforms.Count);
+            Console.WriteLine(engines.Count);
+            Console.WriteLine(dealers.Count);
+            Console.WriteLine(towns.Count);
+
             using (ctx)
             {
-                foreach (Car car in cars)
+                foreach (var town in towns)
+                {
+                    if (!ctx.Towns.Any(c => c.Id == town.Id))
+                    {
+                        ctx.Towns.Add(town);
+                    }
+                }
+
+                foreach (var platform in platforms)
+                {
+                    if (!ctx.Platforms.Any(c => c.Id == platform.Id))
+                    {
+                        ctx.Platforms.Add(platform);
+                    }
+                }
+
+                foreach (var order in orders)
+                {
+                    if (!ctx.Orders.Any(c => c.Id == order.Id))
+                    {
+                        ctx.Orders.Add(order);
+                    }
+                }
+
+                foreach (var manufacturer in manufacturers)
+                {
+                    if (!ctx.Manufacturers.Any(c => c.Id == manufacturer.Id))
+                    {
+                        ctx.Manufacturers.Add(manufacturer);
+                    }
+                }
+
+                foreach (var engine in engines)
+                {
+                    if (!ctx.Engines.Any(c => c.Id == engine.Id))
+                    {
+                        ctx.Engines.Add(engine);
+                    }
+                }
+
+                foreach (var model in models)
+                {
+                    if (!ctx.Models.Any(c => c.Id == model.Id))
+                    {
+                        ctx.Models.Add(model);
+                    }
+                }
+
+                foreach (var dealer in dealers)
+                {
+                    if (!ctx.Dealers.Any(d => d.Id == dealer.Id))
+                    {
+                        ctx.Dealers.Add(dealer);
+                    }
+                }
+
+                foreach (var car in cars)
                 {
                     if (!ctx.Cars.Any(c => c.Id == car.Id))
                     {
@@ -62,7 +137,6 @@
                     }
                 }
 
-                // TO FIX: Method returns error for foreign key.
                 ctx.SaveChanges();
             }
         }
--- CarsFactory/CarsFactory.MongoDb.Data/MongoDbRepository.cs
+++ CarsFactory/CarsFactory.MongoDb.Data/MongoDbRepository.cs
@@ -16,6 +16,13 @@ namespace CarsFactory.MongoDb.Data
         private static readonly IMongoDatabase Database = Client.GetDatabase("factory");
 
         private static readonly IMongoCollection<BsonDocument> CarsCollection = MbDatabase.GetCollection<BsonDocument>("Cars");
+        private static readonly IMongoCollection<BsonDocument> DealersCollection = MbDatabase.GetCollection<BsonDocument>("Dealers");
+        private static readonly IMongoCollection<BsonDocument> EnginesCollection = MbDatabase.GetCollection<BsonDocument>("Engines");
+        private static readonly IMongoCollection<BsonDocument> ManufacturersCollection = MbDatabase.GetCollection<BsonDocument>("Manufacturers");
+        private static readonly IMongoCollection<BsonDocument> ModelsCollection = MbDatabase.GetCollection<BsonDocument>("Models");
+        private static readonly IMongoCollection<BsonDocument> OrdersCollection = MbDatabase.GetCollection<BsonDocument>("Orders");
+        private static readonly IMongoCollection<BsonDocument> PlatformsCollection = MbDatabase.GetCollection<BsonDocument>("Platforms");
+        private static readonly IMongoCollection<BsonDocument> TownsCollection = MbDatabase.GetCollection<BsonDocument>("Towns");
 
         public static IMongoDatabase MbDatabase
         {
@@ -33,5 +40,60 @@ namespace CarsFactory.MongoDb.Data
             return cars;
         }
 
+        public async Task<IList<Manufacturer>> GetManufacturersData()
+        {
+            var manufacturers = (await ManufacturersCollection.Find(new BsonDocument()).ToListAsync())
+                .Select(bs => BsonSerializer.Deserialize<Manufacturer>(bs)).ToList();
+
+            return manufacturers;
+        }
+
+        public async Task<IList<Dealer>> GetDealersData()
+        {
+            var dealers = (await DealersCollection.Find(new BsonDocument()).ToListAsync())
+                .Select(bs => BsonSerializer.Deserialize<Dealer>(bs)).ToList();
+
+            return dealers;
+        }
+
+        public async Task<IList<Engine>> GetEnginesData()
+        {
+            var engines = (await EnginesCollection.Find(new BsonDocument()).ToListAsync())
+                .Select(bs => BsonSerializer.Deserialize<Engine>(bs)).ToList();
+
+            return engines;
+        }
+
+        public async Task<IList<Model>> GetModelsData()
+        {
+            var models = (await ModelsCollection.Find(new BsonDocument()).ToListAsync())
+                .Select(bs => BsonSerializer.Deserialize<Model>(bs)).ToList();
+
+            return models;
+        }
+
+        public async Task<IList<Order>> GetOrdersData()
+        {
+            var orders = (await OrdersCollection.Find(new BsonDocument()).ToListAsync())
+                .Select(bs => BsonSerializer.Deserialize<Order>(bs)).ToList();
+
+            return orders;
+        }
+
+        public async Task<IList<Platform>> GetPlatformsData()
+        {
+            var platforms = (await PlatformsCollection.Find(new BsonDocument()).ToListAsync())
+                .Select(bs => BsonSerializer.Deserialize<Platform>(bs)).ToList();
+
+            return platforms;
+        }
+
+        public async Task<IList<Town>> GetTownsData()
+        {
+            var towns = (await TownsCollection.Find(new BsonDocument()).ToListAsync())
+                .Select(bs => BsonSerializer.Deserialize<Town>(bs)).ToList();
+
+            return towns;
+        }
     }
 }
