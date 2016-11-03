718564c3e7d87b914e96af3b9ea8299cb95df2a6
Alex Nestorov <nestorov.alexander@gmail.com>
2016-11-03 22:19:14 +0200

    add SQLite project -- in progress

---
+++ CarsFactory/CarsFactory.SQLite.Data/SQLiteRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+using CarsFactory.DtoModels;
+
+namespace CarsFactory.SQLite.Data
+{
+    public class SQLiteRepository
+    {
+        public IList<TownInfoDto> GetTownsData()
+        {
+            // TODO:
+            return new List<TownInfoDto>();
+        }
+    }
+}
